d9fcf67c8606b4b5cb55a051c6bd41d92fe63204
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2022-11-02 09:36:57 -0300

    join basic form transfer

--- scripts/join/main.js
+++ scripts/join/main.js
@@ -6,6 +6,12 @@ const selectors = document.querySelectorAll(".login__step__selector")
 const step_titles = ['Join the brotherhood', 'Just a more few steps...', 'Sign with blood!']
 const step_title_el = document.querySelector('.login__title')
 
+const fake_nome = document.getElementById('fake_name')
+const fake_email = document.getElementById('fake_email')
+const fake_senha = document.getElementById('fake_password')
+const fake_cpf = document.getElementById('fake_cpf')
+const fake_form = [fake_nome, fake_email, fake_senha, fake_cpf]
+
 const nome = document.getElementById('nome')
 const email = document.getElementById('email')
 const senha = document.getElementById('senha')
@@ -17,6 +23,22 @@ let counter = 0
 selectors[0].classList.toggle('login__step__selector_active')
 step_title_el.innerText = step_titles[0]
 
+
+function onChangeTransferValue(input, output) {
+    input.addEventListener('change', (e)=> {
+        console.log('INPUT: ', input.value, input.innerText)
+        output.value = input.value
+        output.innerText = input.innerText
+        console.log('OUTPUT: ', output.value, output.innerText)
+    })
+}
+
+function transferValueForAll(input, output) {
+    for (let x = 0; x < input.length; x++) {
+        console.log('ELS (IN | OUT): ', input[x], output[x])
+        onChangeTransferValue(input[x], output[x])
+    }
+}
 
 function roll() {
     btn.forEach(btn => {
@@ -35,11 +57,13 @@ function roll() {
     })
 }
 
-btn_submit.addEventListener('click', ()=> {
-    modal.style.transform = `translateY(0%)`
-    modal.style.opacity = 1
-})
+// btn_submit.addEventListener('click', ()=> {
+//     modal.style.transform = `translateY(0%)`
+//     modal.style.opacity = 1
+// })
 
+transferValueForAll(fake_form, form)
+// onChangeTransferValue(fake_senha, senha)
 roll()
 
 
