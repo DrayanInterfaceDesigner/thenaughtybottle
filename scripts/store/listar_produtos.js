007596df045a363feb003d2e9b0fdb90621d4104
srta-raimee <107631584+srta-raimee@users.noreply.github.com>
2022-11-07 15:54:02 -0300

    Descrição produtos

--- scripts/store/listar_produtos.js
+++ scripts/store/listar_produtos.js
@@ -8,6 +8,6 @@ fetch("../php/listar_produtos.php",{
 
 function listar(produto){
     for(var i = 0; i < produto.length; i++){
-        document.getElementById("produtos").innerHTML += produto[i].nome +"<br>"+produto[i].valor+"<br><br>";
+        document.getElementById("produtos").innerHTML += produto[i].id_produto+ "<br>"+produto[i].nome +"<br>"+produto[i].valor+"<br>"+produto[i].estoque+"<br>"+produto[i].descricao+"<br>"+produto[i].model_addres+"<br>"+produto[i].cor+"<br><br>";
     }
 }
