a74917c6818cd07422bcde0c76b98f6517773715
srta-raimee <107631584+srta-raimee@users.noreply.github.com>
2022-11-07 09:28:54 -0300

    login implementado

---
+++ php/listar.php
@@ -0,0 +1,19 @@
+<?php
+include "conexao.php";
+$select = mysqli_query($conn, "SELECT * FROM produtos");
+
+$i = 0;
+
+while($registro = mysqli_fetch_assoc($select)){
+    $resultado[$i]["id_produto"] = $registro["id_produto"];
+    $resultado[$i]["nome"] = $registro["nome"];
+    $resultado[$i]["valor"] = $registro["valor"];
+    $resultado[$i]["estoque"] = $registro["estoque"];
+    $resultado[$i]["descricao"] = $registro["descricao"];
+    $resultado[$i]["model_address"] = $registro["model_address"];
+    $resultado[$i]["cor"] = $registro["cor"];
+
+}
+
+$objJSON = json_encode($resultado);
+echo $objJSON;
--- php/login.php
+++ php/login.php
@@ -3,10 +3,10 @@ require("conexao.php");
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 $select = mysqli_query($conn, "SELECT * FROM clientes WHERE email = '$email' and senha = '$senha'");
-
+echo $email;
 
 if(mysqli_num_rows($select) == 1){
-    echo "<script defer>alert('Bem-vindo!')</script>";
+    echo "<script>alert('Bem-vindo!')</script>";
     $usuario = $select->fetch_assoc();
     if(!isset($_SESSION)){
         session_start();
@@ -14,7 +14,8 @@ if(mysqli_num_rows($select) == 1){
     $_SESSION['id'] = $usuario['id_clientes'];
     $_SESSION['nome'] = $usuario['nome'];
 
-    header("location: ../pages/home.htm");
+    header("location: ../pages/home_welcome.htm");
+
 }
 else{
     echo "<script>alert('Digite uma senha válida!')</script>";
