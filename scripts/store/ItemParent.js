950fb1b3b3996fc44baf36ddaf5fdbb2fd028079
Drayan Silva Magalhães <31392046+DrayanInterfaceDesigner@users.noreply.github.com>
2022-11-08 16:57:33 -0300

    OnClickBuy

--- scripts/store/ItemParent.js
+++ scripts/store/ItemParent.js
@@ -47,12 +47,74 @@ export class ItemParent {
         const buy__button__wrapper = this.#createEl('div', 'buy__button__wrapper', item__buy__wrapper)
         const buy__button = this.#createEl('div', 'buy__button', buy__button__wrapper)
         const buy__button__label = this.#createEl('p', 'buy__button__label', buy__button)
-        .innerText = 'Buy'
+        buy__button__label.innerText = 'Buy'
         const buy__price = this.#createEl('div', 'buy__price', item__buy__wrapper)
         const price__tag = this.#createEl('p', 'price__tag', buy__price)
         .innerText = item.price
         const price__currency = this.#createEl('p', 'price__currency', buy__price)
         .innerText = 'BRL'
+
+        item__buy__wrapper.addEventListener("click", ()=> {
+            item__buy__wrapper.classList.remove("item__buy__wrapper")
+            buy__button__wrapper.classList.remove("buy__button__wrapper")
+            buy__button.classList.remove("buy__button")
+            item__buy__wrapper.classList.add("item__buy__wrapper__disabled")
+            buy__button__wrapper.classList.add("buy__button__wrapper__disabled")
+            buy__button.classList.add("buy__button__disabled")
+
+            product__list__item.classList.remove("products__list__item")
+            product__list__item.classList.add("products__list__item__disabled")
+            product__list__item.classList.add("disabled")
+            buy__button__label.innerText = "Bought"
+        })
+
+        return item__3D__container
+    }
+
+    createForBag(item) {
+        const product__list__item = this.#createListItem()
+        const item__3D__container = this.#createEl('div', 'item__3D__container', product__list__item)
+        const item__header = this.#createEl('div', 'item__header', product__list__item)
+        const item__footer = this.#createEl('div', 'item__footer', product__list__item)
+
+        product__list__item.setAttribute("id", `productID_${item.id}`)
+        const item__header__info = this.#createEl('div', 'item__header__info', item__header)
+        const info__name = this.#createEl('p', 'info__name', item__header__info)
+        .innerText = item.name
+        const info__subtitle = this.#createEl('p', 'info__subtitle', item__header__info)
+        .innerText = item.subtitle
+        const info__bg__wrapper = this.#createEl('div', 'info__bg__wrapper', item__header__info)
+        const info__bg = this.#createEl('img', 'info__bg', info__bg__wrapper)
+        .setAttribute('src', '../assets/svg/petals.svg')
+        const info__bg_2 = this.#createEl('img', 'info__bg', info__bg__wrapper)
+        .setAttribute('src', '../assets/svg/petals.svg')
+
+        const item__description = this.#createEl('p', 'item__description', item__footer)
+        .innerText = item.description
+        const item__buy__wrapper = this.#createEl('div', 'item__buy__wrapper', item__footer)
+        const buy__button__wrapper = this.#createEl('div', 'buy__button__wrapper', item__buy__wrapper)
+        const buy__button = this.#createEl('div', 'buy__button', buy__button__wrapper)
+        const buy__button__label = this.#createEl('p', 'buy__button__label', buy__button)
+        buy__button__label.innerText = 'Buy'
+        const buy__price = this.#createEl('div', 'buy__price', item__buy__wrapper)
+        const price__tag = this.#createEl('p', 'price__tag', buy__price)
+        .innerText = item.price
+        const price__currency = this.#createEl('p', 'price__currency', buy__price)
+        .innerText = 'BRL'
+
+        item__buy__wrapper.addEventListener("click", ()=> {
+            item__buy__wrapper.classList.remove("item__buy__wrapper")
+            buy__button__wrapper.classList.remove("buy__button__wrapper")
+            buy__button.classList.remove("buy__button")
+            item__buy__wrapper.classList.add("item__buy__wrapper__disabled")
+            buy__button__wrapper.classList.add("buy__button__wrapper__disabled")
+            buy__button.classList.add("buy__button__disabled")
+
+            product__list__item.classList.remove("products__list__item")
+            product__list__item.classList.add("products__list__item__disabled")
+            product__list__item.classList.add("disabled")
+            buy__button__label.innerText = "Bought"
+        })
 
         return item__3D__container
     }
