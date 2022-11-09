a360eb6683ba2ad9528ce039580f3f1146b0d3b3
srta-raimee <107631584+srta-raimee@users.noreply.github.com>
2022-11-08 23:21:08 -0300

    nome

--- php/adiciona-carrinho.php
+++ php/adiciona-carrinho.php
@@ -2,17 +2,20 @@
     include "conexao.php";
 
     //índice do produto no array dados[]
-    $cod = $_POST["id_produto"];
+    //$cod = json_decode($_POST["id_produto"]);
+    $cod = json_decode(file_get_contents('php://input'), true);
+    //print_r($cod);
+    echo $cod["id_produto"];
 
     $query = mysqli_query($conn, "SELECT * FROM carrinho WHERE id_carrinho = '$cod'");
     $carrinho = mysqli_fetch_assoc($query);
-
-    //se o produto já está no carrinho
+    echo "$carrinho";
+    // //se o produto já está no carrinho
     if($carrinho){
-        mysqli_query($conn, "UPDATE carrinho SET quantidade = quantidade + 1 WHERE id_carrinho = $cod");
+        mysqli_query($conn, "UPDATE carrinho SET quantidade = quantidade + 1 WHERE id_carrinho = '$cod'");
     }
     //produto não está no carrinho
     else{
-        mysqli_query($conn, "INSERT INTO carrinho(id_carrinho, quantidade) VALUE ($cod, 1)");
+        mysqli_query($conn, "INSERT INTO carrinho(id_carrinho, quantidade) VALUE ('$cod', 1)");
     }
 ?>
