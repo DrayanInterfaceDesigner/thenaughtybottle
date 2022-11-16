f98c10e23ff3aac9ceb3feb0b65c299286bfe457
srta-raimee <107631584+srta-raimee@users.noreply.github.com>
2022-11-16 10:34:14 -0300

    inserçoes

--- bd/teste.sql
+++ bd/teste.sql
@@ -6,3 +6,7 @@ CREATE TABLE usuarios(
   senha VARCHAR(20) NOT NULL, 
   email VARCHAR(40) NOT NULL, 
   PRIMARY KEY(id));
+
+INSERT INTO users.usuarios(nome_user, senha, email) 
+VALUES('Jobersvaldo', '1234','llll@gmail.com'),
+('Reinaldo', '12345', 'naosei@gmail.com');
